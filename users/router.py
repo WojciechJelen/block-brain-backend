1c13d9139493b5a846f9e8c1382d9fb92f085e51
Wojciech Jelen <wjelen30@gmail.com>
2025-05-29 22:54:09 +0200

    Refactor user router to remove duplicate '/me' endpoint definition

--- users/router.py
+++ users/router.py
@@ -4,10 +4,11 @@ from typing import Dict
 
 user_router = APIRouter(prefix="/users")
 
+@user_router.get("/me")
+async def get_me(user: Dict = Depends(get_current_user)):
+    return user
+
 @user_router.get("/{user_id}")
 async def get_user(user_id: int):
     return {"user_id": f"{user_id}"}
 
-@user_router.get("/me")
-async def get_me(user: Dict = Depends(get_current_user)):
-    return user
